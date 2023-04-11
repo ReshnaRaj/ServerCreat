6e9a8f5b10cb97b96b31dcbe72e644ab6850c6c7
ReshnaRaj <reshnarajan84@gmail.com>
2023-04-11 16:16:40 +0530

    server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const app=express()
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.listen(6500,()=>{
     console.log("localhost host runnng on 6500 port");
 })
