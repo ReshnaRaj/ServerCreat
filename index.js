482f8e6a7a359af2744e3ecd56334f08c009dbd5
ReshnaRaj <reshnarajan84@gmail.com>
2023-04-11 16:12:15 +0530

    server creation

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express()
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+app.listen(6500,()=>{
+    console.log("localhost host runnng on 6500 port");
+})
